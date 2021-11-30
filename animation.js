9140da5b7b42d745952b644693366201f235b213
MW Softwise <mwsoftwise@gmail.com>
2021-11-30 14:23:24 +0100

    Finished website with mailing,  animations and english version

--- animation.js
+++ animation.js
@@ -7,3 +7,34 @@ burger.addEventListener('click', () => {
     burger.classList.toggle('active')
     menu.classList.toggle('active')
 })
+
+// MENU SCROLLING
+
+$('nav ul li a').on("click", function () {
+    const scrolling = "#" + $(this).attr('data-menu');
+
+    $('body, html').animate({
+        scrollTop: $(scrolling).offset().top - 80
+    })
+})
+$('nav aside').on("click", function () {
+    const scrolling = "#" + $(this).attr('class');
+
+    $('body, html').animate({
+        scrollTop: $(scrolling).offset().top
+    })
+})
+
+// ANIMATIONS
+gsap.registerPlugin(ScrollTrigger);
+
+const fromTop = document.querySelectorAll('.animateTop');
+
+
+
+fromTop.forEach(item =>{
+    gsap.fromTo(item.children, {y:'+=100',opacity: 0}, {y: 0, opacity: 1, stagger: 0.3, duration:1, ease:'easeInOut', scrollTrigger:{
+        trigger: item,
+        start: 'top 53%'
+    }});
+});
---
+++ mail.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MW Softwise</title>
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="mailing.css">
+    
+</head>
+<body>
+
+<header>
+<?php 
+
+$to     = 'mwsoftwise@gmail.com';
+$name   = $_POST['name'];
+$email  = $_POST['email'];
+$subject= 'Nowy e-mail od' . $name . '(' . $email . ')';
+$message= $_POST['msg'];
+$headers= 'From ' . $name . '(' . $email . ')';
+$headers= "Content-Type: text/html; charset=utf-8\r\n";
+
+mail($to, $subject, $message, $headers);
+?>
+    <h1>Wiadomość została wysłana</h1>
+    <a href="index.html"><button>Powrót na stronę główną</button></a>
+</header>
+    
+</body>
+</html>
+
